7dd1c54f47a337c4ff1a9bc497cb8feedc5e5910
Vivek Babu <vivekbabu0105@gmail.com>
2024-03-09 12:48:23 +0530

    paginator implementing in products page

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . models import Product
+from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -9,6 +10,8 @@ def list_products(request):
     return products list page
     """
     product_list=Product.objects.all()
+    product_paginator=Paginator(product_list,2)
+    product_list=product_paginator.get_page(1)
     context={'products':product_list}
     return render(request,'products.html',context)
 def detail_product(request):
