e7155bca9a455401b580993ff128ac280708d9b3
Vivek Babu <vivekbabu0105@gmail.com>
2024-03-13 00:46:26 +0530

    registration and login completed

--- customers/views.py
+++ customers/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
 from . models import Customer
 from  django.contrib import messages
 # Create your views here.
@@ -13,7 +14,7 @@ def show_account(request):
             phone=request.POST.get('phone')
             #user account creation
 
-            user=User.objects.create(
+            user=User.objects.create_user(
                 username=username,
                 password=password,
                 email=email
@@ -23,11 +24,23 @@ def show_account(request):
             Customer.objects.create(
                 user=user,
                 phone=phone,
-                addres=addres
+                address=address
             )
-            return redirect('home')
+            success_message="user registered successfully"
+            messages.success(request,success_message)
+            #return redirect('home')
         except Exception as e:
-            error_message="Duplication or invalid user"
+            error_message="Duplication or invalid input"
             messages.error(request,error_message)
+    if request.POST and 'login' in request.POST:
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(username=username,password=password) #authentication take place in this line validating the user and pass if there exist return a user obj
+        if user:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'invalid user credential')
+
 
     return render(request,'account.html')
