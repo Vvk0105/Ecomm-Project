776144f246e935d2f67dde26da2485f2fb74612e
Vivek Babu <vivekbabu0105@gmail.com>
2024-03-14 19:55:18 +0530

    order management completed

--- orders/views.py
+++ orders/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render,redirect
 from . models import Order,OrderedItem
 from products.models import Product
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def show_cart(request):
     user=request.user
     customer=user.customer_profile
+    print('user',user)
+    print('customer',customer)
     cart_obj,create=Order.objects.get_or_create(
         owner=customer,
         order_status=Order.CART_STAGE
@@ -45,7 +48,7 @@ def checkout_cart(request):
 
 
     
-
+@login_required(login_url='account')
 def add_to_cart(request):
     if request.POST:
         user=request.user
@@ -69,3 +72,12 @@ def add_to_cart(request):
             ordered_item.quantity+=quantity
             ordered_item.save()
     return redirect('cart')
+
+@login_required(login_url='account')
+def show_orders(request):
+    user=request.user
+    customer=user.customer_profile
+    all_orders=Order.objects.filter(owner=customer).exclude(order_status=Order.CART_STAGE)
+    context={'orders':all_orders}
+    
+    return render(request,'orders.html',context)
